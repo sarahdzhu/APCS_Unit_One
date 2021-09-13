354f053daafc734d8182f90076d03cf4d3cd3363
sarahdzhu <zhudo@ssfs.org>
2021-09-13 12:10:12 -0400

    Correct Change

--- src/CorrectChange.java
+++ src/CorrectChange.java
@@ -4,5 +4,37 @@ public class CorrectChange {
 
     public static void main(String[] args) {
 
+  int dollar;
+  int dollarRemain;
+  int quarter;
+  int quarterRemain;
+  int dime;
+  int dimeRemain;
+  int nickle;
+  int nickleRemain;
+  int penny;
+
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Please enter a whole number of cents:");
+
+  int change= scan.nextInt();
+
+  dollar=change /100; //1
+        dollarRemain=change %100; //63
+        quarter= dollarRemain/ 25; //2
+        quarterRemain=dollarRemain%25; //13
+        dime=quarterRemain/ 10;
+        dimeRemain=quarterRemain%10; //25
+        nickle=dimeRemain/5;
+        nickleRemain=dimeRemain %5;
+        penny=dimeRemain/1;
+
+        System.out.println("You have "+dollar+" dollars, "+quarter+" quarters, "+dime+" dimes, "+nickle+" nickles, and "+penny+" pennies.");
+
+
+
+
+
     }
 }
+
