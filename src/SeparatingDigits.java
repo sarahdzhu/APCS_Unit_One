6dcf8140820f6a66f8b07a8ed31cc6c3738379b2
sarahdzhu <zhudo@ssfs.org>
2021-09-13 14:59:42 -0400

    SeparatingDigits

--- src/SeparatingDigits.java
+++ src/SeparatingDigits.java
@@ -1,6 +1,25 @@
-public class SeparatingDigits {
+import java.util.Scanner;
+ public class SeparatingDigits {
 
     public static void main(String[] args) {
+
+Scanner scan=new Scanner(System.in);
+System.out.println("Type the number: ");
+int number=scan.nextInt();
+
+int first= number / 10000;
+int firstRemain= number % 10000;
+int second= firstRemain / 1000;
+int secondRemain= number % 1000;
+int third= secondRemain / 100;
+int thirdRemain= number % 100;
+int forth= thirdRemain / 10;
+int forthRemain= number % 10;
+int fifth =forthRemain/ 1;
+
+System.out.println(first+" "+second+" "+third+" "+forth+" "+fifth);
+
+
 
     }
 }
