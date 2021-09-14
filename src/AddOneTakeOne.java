e70c887ffa4e7546e9be39984a22075e33e20378
sarahdzhu <zhudo@ssfs.org>
2021-09-14 15:03:03 -0400

    1.4 exercises

--- src/AddOneTakeOne.java
+++ src/AddOneTakeOne.java
@@ -1,6 +1,18 @@
+
+
+        import java.util.Scanner;
 public class AddOneTakeOne {
 
     public static void main(String[] args) {
+Scanner scan=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int value= scan.nextInt();
+        int value1=value-1;
+        int value2=value;
+        int value3=value+1;
+        System.out.println(value1+" "+ value2+" " +value3);
+
+
 
     }
 }
--- src/FindTheAverage.java
+++ src/FindTheAverage.java
@@ -4,7 +4,41 @@ public class FindTheAverage {
 
     public static void main(String[] args) {
 
-        // Create a Scanner object to get user input
+       Scanner scan=new Scanner(System.in);
+       System.out.println("Enter four values.");
+       System.out.print("Value 1: ");
+       double value1= scan.nextDouble();
+       System.out.print("Value 2: ");
+       double value2=scan.nextDouble();
+       System.out.print("Value 3: ");
+       double value3=scan.nextDouble();
+       System.out.print("Value 4: ");
+       double value4=scan.nextDouble();
+
+       double total=value1+value2+value3+value4;
+       double average=total/4;
+
+       System.out.println("Average = "+average);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       // Create a Scanner object to get user input
 
         // Create a total variable to keep track of the total value
 
