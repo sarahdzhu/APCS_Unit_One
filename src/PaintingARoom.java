c2028585673eb0279f14037d6bc13d9ddafc0705
sarahdzhu <zhudo@ssfs.org>
2021-09-17 09:57:06 -0400

    painting room

--- src/PaintingARoom.java
+++ src/PaintingARoom.java
@@ -1,6 +1,29 @@
+
+import java.util.Scanner;
 public class PaintingARoom {
 
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Find out how many gallons of paint you need to paint your room!");
+        System.out.println("Please enter the following values.");
+        System.out.print("Length(ft): ");
+        double length=scan.nextDouble();
+        System.out.print("Width(ft): ");
+        double width=scan.nextDouble();
+        System.out.print("Height(ft): ");
+        double height=scan.nextDouble();
+        System.out.print("Number of doors: ");
+        int door=scan.nextInt();
+        System.out.print("Number of windows: ");
+        int window=scan.nextInt();
+        double surfaceArea=(2*width*length+2*height*length+2*height*width)-(15*door-10*window);
+        double gallonPaint=surfaceArea/350;
+        int roundedGallon=(int)(-(-gallonPaint));
+        System.out.println("You will need "+roundedGallon+" gallons of paint!");
+        System.out.println("You should've just hired someone to paint your room!");
+
+
+
 
     }
 
